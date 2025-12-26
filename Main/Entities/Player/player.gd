222757c822e87e234c6a484005a0f76bc3500c0e
Noah <ngoodmonster@gmail.com>
2025-12-26 15:11:05 -0800

    added paralax background and a quick unfinish starting 'animation'

--- Main/Entities/Player/player.gd
+++ Main/Entities/Player/player.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 const JUMP_VELOCITY = -50
 
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if position <= Vector2(192, 0): 
+		velocity.x += 1 
+	else: 
+		velocity.x = 0 
+		
+
 
 	# Handle jump.
 	if Input.is_action_pressed("ui_accept") and velocity.y > -500:
---
+++ Main/parallax_2d.gd
@@ -0,0 +1,5 @@
+extends Parallax2D
+
+
+func _on_timer_timeout() -> void:
+	$Img0006.visible = false
