3200a99e40b3e4da38129abd54b1455063cf7e06
Noah <107080597+RichardBlevins@users.noreply.github.com>
2025-12-29 01:17:38 -0800

    Thorns and death
    - added thorns scene
    - implemented player interaction hitbox

--- Main/Entities/Player/player.gd
+++ Main/Entities/Player/player.gd
@@ -6,6 +6,8 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+		#start sequence ...
 	if position <= Vector2(192, 0): 
 		velocity.x = 300
 	else: 
@@ -13,12 +15,14 @@ func _physics_process(delta: float) -> void:
 		Manager.start = true
 		
 		
-		
-
 
 	# Handle jump.
 	if Input.is_action_pressed("ui_accept") and velocity.y > -500:
 		velocity.y += JUMP_VELOCITY
-
-
 	move_and_slide()
+
+
+
+func _on_hitbox_interact(area: Area2D) -> void:
+	if area.is_in_group("Death"):
+		print("daed")
